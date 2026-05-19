344e8d2bd644f5954ea38855de43091a6365b02a
Microwave <121560464+Micro856@users.noreply.github.com>
2026-05-19 17:12:52 +0200

    Update scis-account-page-local.c

--- scarecrow-initial-setup/pages/account/scis-account-page-local.c
+++ scarecrow-initial-setup/pages/account/scis-account-page-local.c
@@ -38,7 +38,7 @@
 #define GOA_API_IS_SUBJECT_TO_CHANGE
 #include <goa/goa.h>
 
-#include <rest/oauth-proxy.h>
+#include <rest/oauth2-proxy.h>
 #include <json-glib/json-glib.h>
 
 #define VALIDATION_TIMEOUT 600
